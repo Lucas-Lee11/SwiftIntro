f04a68289e3fdcc3776775deb0e5bc6f7d2630ad
Lucas Lee <lucasclee11@gmail.com>
2020-09-24 23:03:33 -0400

    swiftUI

---
+++ Day3/Test/Test/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Lucas Lee on 9/24/20.
+//  Copyright © 2020 Lucas Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        
+    @State var num:Int = 0
+    @State var name:String = ""
+    @State var selected:Int = 0
+    var names:[String] {["lucas", "chris", name]}
+    
+    var body: some View {
+        NavigationView{
+            Form{
+               Section {
+                    Text("Tap Below")
+                    Button("Tapped \(num) times"){
+                        self.num += 1
+                    }
+                    TextField("Whats ur name", text: $name)
+                }
+                Section {
+                    Picker("Select your name", selection: $selected) {
+                        ForEach(0 ..< names.count) {
+                            Text(self.names[$0])
+                        }
+                    }
+                    Text("Hello World")
+                    Text("Hello \(self.names[selected])")
+                }
+            }.navigationBarTitle("SwiftUI")
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
